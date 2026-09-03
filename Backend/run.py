45b862ac22af14a7179e5af7b65e81b9a1c8da64
Jazib Ahmad <140528310+jzahmad@users.noreply.github.com>
2026-09-03 12:07:26 -0300

    eip and made bebug false

--- Backend/run.py
+++ Backend/run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run(debug=True) # for local
-    app.run(host='0.0.0.0', debug=True, port=5000)  # for devel
+    app.run(host='0.0.0.0', debug=False, port=5000)  # for devel
